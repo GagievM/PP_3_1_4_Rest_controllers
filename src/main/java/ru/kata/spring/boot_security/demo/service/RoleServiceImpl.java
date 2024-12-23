87e7237946e886087f007690ae83c4630a798e39
Magomed <gagiev_maga1999@mail.ru>
2024-12-23 21:36:01 +0300

    done

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService{
+
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    public Role getRoleById(Long id){return roleRepository.getRoleById(id);}
+}
